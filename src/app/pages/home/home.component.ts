fe64d8ce436262147f144c128934c58ce277153e
rulo0o10 <rulo.samper@gmail.com>
2019-09-02 12:49:40 -0300

    mobile home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  isMobile: boolean = true;
+  constructor() {}
 
   ngOnInit() {
-  }
+   
+    var mobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    
+    let carousel = document.getElementById("mobile");
+    var maxScrollLeft = carousel.scrollWidth - carousel.clientWidth;
+    carousel.scrollLeft = maxScrollLeft / 2;
+    
+    if(screen.width > 900){
+      this.isMobile = false
+    }
+        
 
+   
+  }
 }
